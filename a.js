13e991790e2509cbadf6591d41b42d923fe4f092
higorkkjx <higorolive2023@gmail.com>
2023-10-30 18:35:23 -0300

    first commit

--- a.js
+++
@@ -1,5 +0,0 @@
-let num = 23;
-if (num % 1 === 0) {  // verifica se o número não tem casas decimais
-  num = num.toFixed(2).replace('.', '.00');  // adiciona ".00" ao final
-}
-console.log(num);  // imprime "23.00"
