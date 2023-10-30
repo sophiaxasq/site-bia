133afa0b52cdad46dfce59ed1dcd8ce533573fb5
higorkkjx <higorolive2023@gmail.com>
2023-10-30 18:27:50 -0300

    att

---
+++ amostra/amostra.php
@@ -0,0 +1,49 @@
+<title>Beatriz souza - sua namoradinha</title>
+<link href="audiovisual 2.css" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Montserrat|Staatliches&display=swap' rel='stylesheet'/>
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:200&display=swap" rel="stylesheet">
+<link rel="icon" type="image/x-icon" href="icon.ico">
+<meta charset="UTF-8">
+<link rel="shortcut icon" type="image/png" href="imgs/CursoProducao.png"/>
+
+	<center>
+		
+		<div id="topo">
+		<br />
+		<h1>BEATRIZ SOUZA</h1>
+		Deixa eu ser sua putinha amor? 🥺
+		</div>
+		<br />
+		<video id="videohere" width="720" height="405" autoplay loop muted>
+			<source src="video.mp4" type="video/mp4">
+			Your browser does not support the video tag.
+		  </video>
+		  <br />
+		<br />
+
+		<form method="get" action="index.php">
+			
+			<button type="submit" class="euquerotopo">QUERO VER A BIA PELADINHA 🔥</button>
+			
+		  </form> 
+		  <div id="depoimentos">
+			<h2>Depoimentos</h2>
+			<div class="depoimento">
+			   <img src="perfil1.jpg" alt="Depoimento 1">
+			   <p>"Meu deus comprei o pack de webnamoro e sai apaixonado kkkkkkk"</p>
+			</div>
+			<div class="depoimento">
+			   <img src="perfil2.jpg" alt="Depoimento 2">
+			   <p>"Nunca bati uma punheta tão gostosa na minha vida rsrs"</p>
+			</div>
+			<div class="depoimento">
+			   <img src="perfil3.jpg" alt="Depoimento 3">
+			   <p>"Que delicia comprei e não me arrependo"</p>
+			</div>
+		 </div>
+		 
+		  <br />
+	
+</center>
+
